2ca27a1bfccb71ba8495d9980a3f9fdc9960612a
John Lerry Taruc <75570239+jltdev15@users.noreply.github.com>
2024-08-23 22:04:58 +0800

    add white list

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(
 );
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://ojtrackingsystem.vercel.app"],
     credentials: true,
   })
 );
